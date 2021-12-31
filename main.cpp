4d4df4430bc2e9bfbebdebed4dd31179b9db3a30
Gioelio <gioele.fiorenza2000@gmail.com>
2021-12-31 13:13:23 +0100

    aggiunti gli iteratori e qualche test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 //
 #include "sparse_matrix.h"
 
+struct predicate{
+    bool operator() (int value){
+        return value < 5;
+    }
+};
+
+template<typename T, typename F>
+int evaluate (sparse_matrix<T> mat, F pred) {
+    unsigned int cont = 0;
+    for(typename sparse_matrix<T>::const_iterator iter = mat.begin(); iter != mat.end(); ++iter) {
+        std::cout << iter->value << pred(iter->value) << std::endl;
+        if (pred(iter->value))
+            cont++;
+    }
+
+    return cont;
+}
+
 void test_fondamentali(){
 
     std::cout << "--- TEST FONDAMENTALI ---" << std::endl;
@@ -52,24 +70,80 @@ void test_sparse_matrix() {
     assert(set(2, 10) == 50);
     assert(set(2, 4) == -1); //valore di default
 
+    sparse_matrix<int> dim(-5, 4, 6);
+
     std::cout << "test column number" << std::endl;
-    assert(set.columns() == 10);
+    assert(set.columns() == 100);
+    assert(dim.columns() == 4);
 
     std::cout << "test rows number" << std::endl;
-    assert(set.rows() == 4);
+    assert(set.rows() == 100);
+    assert(dim.rows() == 6);
 
 }
 
+void test_const_sparse_matrix(const sparse_matrix<int> const_mat) {
 
-int main() {
+    //i metodi di scrittura dovrebbero dare errore
+    //const_mat.set(1, 2, 10);
+
+
+    //i metodi di lettura devono funzionare
+    assert(const_mat(1, 1) == 10);
+    assert(const_mat(2, 2) == -1);
+    assert(const_mat.columns() == 100);
+    assert(const_mat.rows() == 10);
+    assert(const_mat.default_value() == -1);
+    assert(const_mat.size() == 1);
+}
+
+void test_const(){
+    sparse_matrix<int> const_mat(-1, 100, 10);
+
+    const_mat.set(1,1, 10);
+
+    test_const_sparse_matrix(const_mat);
+}
+
+void test_sparse_matrix_iterator() {
+    std::cout << "--- TEST sparse matrix iterator ---" << std::endl;
+    sparse_matrix<int> mat_iter(10, 20, 20);
+    mat_iter.set(1,1, 20);
+    mat_iter.set(1, 2, 30);
+    mat_iter.set(2, 1, 50);
+    mat_iter.set(1,4, 100);
+
+    sparse_matrix<int>::const_iterator iter = mat_iter.begin();
+}
+
+void test_specific_class(){
+
+}
+
+void test_evaluate(){
+    std::cout << "-- TEST evaluate predicate --" << std::endl;
+    sparse_matrix<int> mat(-1, 20, 20);
+
+    //mat.set(1,1, -1);
+    mat.set(2, 2, 6);
+
+    predicate p;
+
+    std::cout << evaluate(mat, p);
+    //assert(evaluate(mat, p) == 1);
+
+}
+
+void execute_tests(){
     test_fondamentali();
     test_sparse_matrix();
+    test_sparse_matrix_iterator();
+    test_const();
+    test_evaluate();
+}
 
-    sparse_matrix<int> a(-3);
-
-    a.set(10, 10, 50);
-
-
+int main() {
+    execute_tests();
 
     return 0;
 }
--- sparse_matrix.h
+++ sparse_matrix.h
@@ -40,18 +40,48 @@ private:
 public:
     /**
      * @brief Costruttore di default
+     *
+     * La dimensione della matrice di default è 100x100
      */
-    sparse_matrix(): _head(nullptr), _size(0), _n_columns(10), _n_rows(10) {}
+    sparse_matrix(): _head(nullptr), _size(0), _n_columns(100), _n_rows(100) {}
+
     /**
      * @brief Costruttore con valore di default passato come argomento
      *
-     * Costruttore che prende un valore di default che viene ritornato quando nella cella da leggere non è presente il valore
+     * Costruttore che imposta il valore di default da ritornare quando non ci sono valori nella cella letta
+     *
+     * La dimensione della matrice di default è 100x100
      *
      * @param default_value
      */
-    explicit sparse_matrix(const value_t &default_value): _default_value(default_value), _head(nullptr), _size(0) {}
+    explicit sparse_matrix(const value_t &default_value): _default_value(default_value), _head(nullptr), _size(0), _n_columns(100), _n_rows(100) {}
 
-    sparse_matrix(const sparse_matrix &other): _head(nullptr), _size(other._size), _default_value(other._default_value) {
+    /**
+     * @brief Costruttore per settare la dimensione e il valore di default della matrice
+     *
+     * Costruttore che consente di scegliere la dimensione della matrice e il suo valore di default
+     *
+     * @param default_value
+     * @param n_cols
+     * @param n_rows
+     */
+    sparse_matrix(const value_t &default_value, const position_t n_cols, const position_t n_rows):
+        _default_value(default_value),
+        _head(nullptr),
+        _size(0),
+        _n_columns(n_cols),
+        _n_rows(n_rows){}
+
+
+
+    /**
+     * @brief Costruttore di copia
+     *
+     * Prende una sparse matrix e ne genera una copia scollegata contenente gli stessi dati di quella originale
+     *
+     * @param other
+     */
+    sparse_matrix(const sparse_matrix &other): _head(nullptr), _size(other._size), _default_value(other._default_value), _n_columns(other._n_columns), _n_rows(other._n_rows) {
 
         element* iter_other = other._head;
 
@@ -67,17 +97,37 @@ public:
         }
     }
 
+    /**
+     * @brief Operatore di assegnamento
+     *
+     * Operatore di assegnamento che serve per copiare il contenuto di other in *this.
+     *
+     * @param other
+     * @return reference a sparse_matrix
+     */
     sparse_matrix& operator=(const sparse_matrix &other){
         if(this != &other){
             sparse_matrix tmp(other);
             std::swap(this->_head, tmp._head);
             std::swap(this->_size, tmp._size);
             std::swap(this->_default_value, tmp._default_value);
+            std::swap(this->_n_rows, tmp._n_rows);
+            std::swap(this->_n_columns, tmp._n_columns);
         }
         return *this;
     }
 
+    /**
+     * @brief Distruttore della classe
+     *
+     * Elimina il contenuto delle celle memorizzate nella matrice
+     *
+     */
     ~sparse_matrix() {
+        _n_rows = 0;
+        _n_columns = 0;
+        _size = 0;
+
         element* current(_head);
 
         //ho preferito fare un while sui puntatori, perché la size indica il numero di celle con un dato valido
@@ -88,22 +138,40 @@ public:
         }
     }
 
+    /**
+     * @brief Ritorna la quantità di dati salvati
+     *
+     * Metodo per ottenere il numero di dati che effettivamente sono stati memorizzati sullo heap
+     *
+     * @return quantità dei dati effettivamente salvati nella matrice
+     */
     size_t size() const { return _size;}
+
+    /**
+     * @brief Ritorna il valore impostato come valore di default
+     *
+     * Metodo per ottenere il dato impostato come dato di default
+     *
+     * @return valore impostato come valore di default
+     */
     const value_t& default_value() const { return _default_value;}
 
-    position_t columns() const {
-        position_t max_column_num = 0;
-        if(_head == nullptr) return 0;
-        element* current = _head;
+    /**
+     * @brief Ritorna il numero delle colonne con cui è stata inizializzata la matrice
+     *
+     * Metodo per conoscere il numero di colonne massime della matrice
+     *
+     * @return numero di colonne della matrice
+     */
+    position_t columns() const { return _n_columns; }
 
-        while(current != nullptr){
-            if(current->y > max_column_num) max_column_num = current->y;
-            current = current->next;
-        }
-
-        return max_column_num;
-    }
-
+    /**
+     * @brief Ritorna il numero delle righe con cui è stata iniziliazzata la matrice
+     *
+     * Metodo per conoscere il numero di righe massime della matrice
+     *
+     * @return
+     */
     position_t rows() const { return _n_rows;}
 
     /**
@@ -117,27 +185,28 @@ public:
      * @param value valore a cui viene impostata la cella indicata in posizione (x, y)
      */
     void set(position_t x, position_t y, const value_t &value){
-        assert(x < _n_rows);
+        assert(x < _n_rows); //TODO: lanciare eccezioni
         assert(y < _n_columns);
 
         element* current(_head);
 
-        while (_size != 0 && current->next != nullptr){
-            if(current->x == x && current->y == y) {
+        if(_size != 0){
+            std::cout << "cerco l'el" << std::endl;
+            while (current->next != nullptr){
+                if(current->x == x && current->y == y) {
+                    current->value = value;
+                    return;
+                }
+                current = current->next;
+            }
+
+            if(current->x == x && current->y == y){
                 current->value = value;
                 return;
             }
-            current = current->next;
-        }
-
-        if(_size != 0 && current->x == x && current->y == y){
-            current->value = value;
-            return;
         }
 
         element* el = new element(x, y, value);
-        if(_m_max < y) _m_max = y;
-        if(_n_max < x) _n_max = x;
 
         if(_size == 0)
             _head = el;
@@ -168,7 +237,91 @@ public:
         return _default_value;
     }
 
+    class const_iterator {
+        //sono chiamati trades dell'iteratore e sono info utili per l'iteratore:
+        // - che tipo di iteratore è
+        // - che tipo di dati contiene
+        // - ptrdiff_t tipo di dato della differenza tra 2 puntatori
+        // gli altri 2 sono puntatori e reference e qui sono settati const, mentre in iterator no.
+    public:
+        typedef std::forward_iterator_tag iterator_category;
+        typedef element                   value_type;
+        typedef ptrdiff_t                 difference_type;
+        typedef const element*            pointer;
+        typedef const element&            reference;
 
+
+        const_iterator(): ptr(nullptr) {}
+
+        const_iterator(const const_iterator &other): ptr(other.ptr) {}
+
+        const_iterator& operator=(const const_iterator &other) {
+            ptr = other.ptr;
+            return *this;
+        }
+
+        //Il distruttore sarà quasi sempre vuoto perchè gli iteratori non possiedono i dati
+        ~const_iterator() {}
+
+        // Ritorna il dato riferito dall'iteratore (dereferenziamento)
+        reference operator*() const {
+            return *ptr;
+        }
+
+        // Ritorna il puntatore al dato riferito dall'iteratore
+        pointer operator->() const {
+            return ptr;
+        }
+
+        // Operatore di iterazione post-incremento
+        const_iterator operator++(int) {
+            const_iterator tmp(*this);
+            *ptr = ptr->next;
+            return tmp;
+        }
+
+        // Operatore di iterazione pre-incremento
+        const_iterator& operator++() {
+            ptr = ptr->next;
+            return *this;
+        }
+
+        // Uguaglianza
+        bool operator==(const const_iterator &other) const {
+            return ptr == other.ptr;
+        }
+
+        // Diversita'
+        bool operator!=(const const_iterator &other) const {
+            return !(ptr == other.ptr);
+        }
+
+    private:
+        //Dati membro
+
+        const element* ptr;
+
+        // La classe container deve essere messa friend dell'iteratore per poter
+        // usare il costruttore di inizializzazione.
+        friend class sparse_matrix;
+
+        // Costruttore privato di inizializzazione usato dalla classe container
+        // tipicamente nei metodi begin e end
+        const_iterator(const element* p): ptr(p) {}
+
+        // !!! Eventuali altri metodi privati
+
+    }; // classe const_iterator
+
+    // Ritorna l'iteratore all'inizio della sequenza dati
+    const_iterator begin() const {
+        return const_iterator(_head);
+    }
+
+    // Ritorna l'iteratore alla fine della sequenza dati
+    const_iterator end() const {
+        return const_iterator(nullptr);
+    }
 
 
 
