7bae1d470950732645b56b888305724f148f1867
Gioelio <gioele.fiorenza2000@gmail.com>
2022-01-09 18:22:15 +0100

    move comment in .h

--- myexception.h
+++ myexception.h
@@ -3,8 +3,16 @@
 
 #include <stdexcept> //std::range_error
 
+/**
+ * Gestione personalizzata delle eccezioni
+ */
 class my_out_of_range: public std::range_error {
 public:
+    /**
+     * @brief Eccezione che viene lanciata quando gli indici escono dalla dimensione logica della matrice
+     *
+     * @param msg messaggio di errore che viene mostrato quando l'eccezione viene lanciata
+     */
     my_out_of_range(const std::string &msg);
 };
 
